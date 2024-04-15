5e7a73b1356dd5618ef4f660f63b1350a78a0076
yusuf <yusufgad24@gmail.com>
2024-04-15 15:34:20 +0300

    fix: deployment error

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://yusuf-ad.github.io/healthcare-system",
+  base: "https://yusuf-ad.github.io/healthcare-system/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
