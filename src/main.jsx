b73f489fb7c8b3a71681ac2f8a55ffbeec4a9dd5
yusuf <yusufgad24@gmail.com>
2024-04-15 15:57:14 +0300

    fix: routing error

--- src/main.jsx
+++ src/main.jsx
@@ -26,14 +26,14 @@ import "./index.css";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
+    <Route path="" element={<App />}>
       <Route index element={<HomePage />} />
 
-      <Route path="/appointments" element={<AppointmentsPage />} />
+      <Route path="appointments" element={<AppointmentsPage />} />
 
-      <Route path="/appointment" element={<AppointmentPage />}>
+      <Route path="appointment" element={<AppointmentPage />}>
         <Route index element={<UserInfoForm />} />
-        <Route path="/appointment/:form" element={<UserInfoForm />} />
+        <Route path="appointment/:form" element={<UserInfoForm />} />
       </Route>
       <Route
         path="*"
